d0c7b82c25fc9a7f00f208a28bd185539eaa8ac2
Tamás Kozman <tunka2000@yahoo.com>
2023-05-23 16:27:54 +0200

    css

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,26 @@
-<script setup lang="ts">
-import TodoForm from './components/TodoForm.vue'
-import TodoList from './components/TodoList.vue'
+<script setup>
+import { ref } from 'vue'
+import ToDoForm from './components/ToDoForm.vue'
+const list = ref([])
+// event handling function triggered by add
+const AddToDo = value => {
+  list.value.push(value)
+}
+const RemoveToDo = value => {
+  list.value.splice(list.value.indexOf(value),1);
+}
+function ClearToDo() {
+  list.value.splice(0);
+}
 </script>
 
 <template>
-  <main>
-
-    <TodoForm/>
-    <TodoList />
-  </main>
+  <div class="child-wrap input-group">
+  <ToDoForm @add="AddToDo"></ToDoForm>
+  <button @click="ClearToDo()">Clear</button>
+  </div>
+  <ul class="parent list-group">
+    <li class="list-group-item" v-for="i in list" :key="i">{{ i }}<button class="remove_button" @click="RemoveToDo(i)">X</button></li>
+  </ul>
+ 
 </template>
-
---
+++ vue-project/src/components/ToDoForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <input 
+      v-model="value"
+      type="text"
+      class="form-control"
+      placeholder="Please enter"
+      @keyup.enter="handleSubmit"
+      ref="text_input"
+      >
+      <button @click="handleSubmit" class="btn btn-primary" type="button">add</button>
+
+</template>
+<script setup>
+import { ref, defineEmits } from 'vue'
+const value = ref('')
+const emits = defineEmits(['add'])
+const handleSubmit = () => {
+  emits('add', value.value)
+  value.value = ''
+}
+import { nextTick } from 'vue';
+</script>
