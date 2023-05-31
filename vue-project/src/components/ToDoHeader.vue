de7c1ec274aa7f5a910d254c1a2260450ca82fb7
Tamás Kozman <tunka2000@yahoo.com>
2023-05-31 10:24:03 +0200

    init

---
+++ vue-project/src/components/ToDoHeader.vue
@@ -0,0 +1,12 @@
+<template>  
+<div class="maindiv">
+    <div class="header">
+        <div>To do list</div>
+        <div>
+            <img src = "../assets/icon_add.svg" height=59.64 alt="Add To DO Item" @click="isModalOpen = true" >
+        </div>
+    </div>
+</div>
+
+</template>
+
